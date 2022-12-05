5b82d6500e017ddb7d706802ff19b840e3b1fc03
d-urbonas <danyu4154@gmail.com>
2022-12-05 12:33:53 -0500

    edited sudoku generator to add more functionality and changed constants

--- Sudoku-Project/sudoku.py
+++ Sudoku-Project/sudoku.py
@@ -53,7 +53,7 @@ def startGame():
     game_over = True
     gameStarted = False
     did_win = False
-    gameButton_font = pygame.font.Font(None, 32)
+    gameButton_font = pygame.font.Font(None, 60)
     button_font = pygame.font.Font(None, 40)
     title_font = pygame.font.Font(None, 64)
     subtitle_font = pygame.font.Font(None, 48)
@@ -68,11 +68,11 @@ def startGame():
     hardText = button_font.render("hard", True, (255, 255, 255), (255, 165, 0))
     hardRect = easyText.get_rect(center=(400, 450))
     resetText = gameButton_font.render("Reset", True, (255, 255, 255), (255, 165, 0))
-    resetRect = resetText.get_rect(center=(150, 650))
+    resetRect = resetText.get_rect(center=(WIDTH/4, 650))
     restartText = gameButton_font.render("Restart", True, (255, 255, 255), (255, 165, 0))
-    restartRect = restartText.get_rect(center=(250, 650))
+    restartRect = restartText.get_rect(center=(2 * WIDTH/4, 650))
     exitText = gameButton_font.render("Exit", True, (255, 255, 255), (255, 165, 0))
-    exitRect = exitText.get_rect(center=(350, 650))
+    exitRect = exitText.get_rect(center=(3 * WIDTH/4, 650))
 
 
 def initialize(cellsToRemove):
@@ -87,6 +87,7 @@ def initialize(cellsToRemove):
     global board
     global gameStarted
     global game_over
+    global sketch_state
 
     gameStarted = True
     game_over = False
@@ -103,6 +104,7 @@ def initialize(cellsToRemove):
         for j, col in enumerate(row):
             initial_sudoku[i][j] = col
     board = sudoku.get_board()
+    sketch_state = [[False for j in range(9)] for i in range(9)]
 
 
 def resetBoard():
@@ -111,6 +113,7 @@ def resetBoard():
     for i, row in enumerate(initial_sudoku):
         for j, col in enumerate(row):
             board[i][j] = col
+
 def main():
     pygame.init()
     startGame()
@@ -138,6 +141,8 @@ def main():
                     if row < 9 and col < 9:
                         print(row, col)
                         print(board[row][col])
+                        if board[row][col] == 0:
+                            sketch_state[row][col] = True
                         screen = pygame.display.set_mode((WIDTH, HEIGHT))
                         pygame.display.set_caption('Sudoku')
                         screen.fill(BG_COLOR)
@@ -151,6 +156,7 @@ def main():
                                          (col * SQUARE_SIZE + SQUARE_SIZE, row * SQUARE_SIZE + SQUARE_SIZE), BOX_WIDTH * 5)
                         pygame.draw.line(screen, RED, (col * SQUARE_SIZE, row * SQUARE_SIZE + SQUARE_SIZE),
                                          (col * SQUARE_SIZE + SQUARE_SIZE, row * SQUARE_SIZE + SQUARE_SIZE), BOX_WIDTH * 5)
+
                     else:
                         if resetRect.collidepoint(event.pos):
                             print("reset")
@@ -213,6 +219,7 @@ def main():
                     draw(screen)
                     continue
 
+
             if event.type == pygame.KEYDOWN and not game_over:
                 if row != -1 and col != -1:
                     sketch_number(screen, event, row, col)
@@ -224,6 +231,8 @@ def main():
                         else:
                             did_win = False
                         game_over = True
+                enter_number(row, col, event)
+                clear(screen, event, row, col)
 
         if game_over and gameStarted:
             pygame.display.update()
@@ -239,7 +248,7 @@ def draw(screen):
         screen.blit(restartText, restartRect)
         screen.blit(exitText, exitRect)
 
-        for i in range(1, BOARD_ROWS):
+        for i in range(0, BOARD_ROWS+1):
             if i % 3 == 0:
                 pygame.draw.line(
                     screen,
@@ -287,26 +296,44 @@ def draw(screen):
 def draw_nums(screen, board):
     # draw a text, 1. define a surface, 2. define the location of the text
     num_font = pygame.font.Font(None, CHIP_FONT)
+    sketch_num_font = pygame.font.Font(None, SKETCH_FONT)
     dict = {
-        1: num_font.render('1', 0, CROSS_COLOR),
-        2: num_font.render('2', 0, CROSS_COLOR),
-        3: num_font.render('3', 0, CROSS_COLOR),
-        4: num_font.render('4', 0, CROSS_COLOR),
-        5: num_font.render('5', 0, CROSS_COLOR),
-        6: num_font.render('6', 0, CROSS_COLOR),
-        7: num_font.render('7', 0, CROSS_COLOR),
-        8: num_font.render('8', 0, CROSS_COLOR),
-        9: num_font.render('9', 0, CROSS_COLOR),
+        1: num_font.render('1', 0, BLACK),
+        2: num_font.render('2', 0, BLACK),
+        3: num_font.render('3', 0, BLACK),
+        4: num_font.render('4', 0, BLACK),
+        5: num_font.render('5', 0, BLACK),
+        6: num_font.render('6', 0, BLACK),
+        7: num_font.render('7', 0, BLACK),
+        8: num_font.render('8', 0, BLACK),
+        9: num_font.render('9', 0, BLACK),
+    }
+    sketch_dict = {
+        1: sketch_num_font.render('1', 0, GREY),
+        2: sketch_num_font.render('2', 0, GREY),
+        3: sketch_num_font.render('3', 0, GREY),
+        4: sketch_num_font.render('4', 0, GREY),
+        5: sketch_num_font.render('5', 0, GREY),
+        6: sketch_num_font.render('6', 0, GREY),
+        7: sketch_num_font.render('7', 0, GREY),
+        8: sketch_num_font.render('8', 0, GREY),
+        9: sketch_num_font.render('9', 0, GREY),
     }
     # draw a text, 1. define a surface, 2. define the location of the text
 
     for row in range(BOARD_ROWS):
         for col in range(BOARD_COLS):
             if board[row][col] != 0:
-                chip_x_surf = dict[board[row][col]]
-                chip_x_rect = chip_x_surf.get_rect(
-                    center=(SQUARE_SIZE * col + SQUARE_SIZE // 2, SQUARE_SIZE * row + SQUARE_SIZE // 2))
-                screen.blit(chip_x_surf, chip_x_rect)
+                if sketch_state[row][col]:
+                    chip_x_surf = sketch_dict[board[row][col]]
+                    chip_x_rect = chip_x_surf.get_rect(
+                        center=(SQUARE_SIZE * col + SQUARE_SIZE // 4, SQUARE_SIZE * row + SQUARE_SIZE // 4))
+                    screen.blit(chip_x_surf, chip_x_rect)
+                else:
+                    chip_x_surf = dict[board[row][col]]
+                    chip_x_rect = chip_x_surf.get_rect(
+                        center=(SQUARE_SIZE * col + SQUARE_SIZE // 2, SQUARE_SIZE * row + SQUARE_SIZE // 2))
+                    screen.blit(chip_x_surf, chip_x_rect)
 
 
 def sketch_number(screen, event, row, col):
@@ -352,17 +379,40 @@ def sketch_number(screen, event, row, col):
             draw_nums(screen, board)
 
 
-def enter_number():
-    pass
+def enter_number(row, col, event):
+    if initial_sudoku[row][col] != 0:
+        return None
+    else:
+        if event.key == pygame.K_RETURN:
+            sketch_state[row][col] = False
+            screen = pygame.display.set_mode((WIDTH, HEIGHT))
+            pygame.display.set_caption('Sudoku')
+            screen.fill(BG_COLOR)
+            draw(screen)
+            draw_nums(screen, board)
 
 
-def clear(self):
-    if event.key == pygame.K_BACKSPACE:
-        board.mark_square(row, col, '-')
-        screen.fill(BG_COLOR)
-        board.draw()
-        pygame.display.update()
 
+def clear(screen, event, row, col):
+    if initial_sudoku[row][col] != 0:
+        return None
+    else:
+        if event.key == pygame.K_BACKSPACE:
+            board[row][col] = 0
+            screen = pygame.display.set_mode((WIDTH, HEIGHT))
+            pygame.display.set_caption('Sudoku')
+            screen.fill(BG_COLOR)
+            draw(screen)
+            draw_nums(screen, board)
+            pygame.draw.line(screen, RED, (col * SQUARE_SIZE, row * SQUARE_SIZE),
+                             (col * SQUARE_SIZE + SQUARE_SIZE, row * SQUARE_SIZE), BOX_WIDTH * 5)
+            pygame.draw.line(screen, RED, (col * SQUARE_SIZE, row * SQUARE_SIZE),
+                             (col * SQUARE_SIZE, row * SQUARE_SIZE + SQUARE_SIZE), BOX_WIDTH * 5)
+            pygame.draw.line(screen, RED, (col * SQUARE_SIZE + SQUARE_SIZE, row * SQUARE_SIZE),
+                             (col * SQUARE_SIZE + SQUARE_SIZE, row * SQUARE_SIZE + SQUARE_SIZE), BOX_WIDTH * 5)
+            pygame.draw.line(screen, RED, (col * SQUARE_SIZE, row * SQUARE_SIZE + SQUARE_SIZE),
+                             (col * SQUARE_SIZE + SQUARE_SIZE, row * SQUARE_SIZE + SQUARE_SIZE), BOX_WIDTH * 5)
+            print(board)
 
 def check_if_victory():
     # checks to see if the player has won sudoku
@@ -377,7 +427,6 @@ def check_if_full():
         for col in row:
             if col == 0:
                 return False
-
     return True
 
 
